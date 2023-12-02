e13a51ef0ab1eaf581ff5bdf443e191d5db0b784
Hugo Cachon <hugo.cachon@hetic.net>
2023-12-02 11:45:14 +0100

    data: added in memory h2 database

--- src/main/java/dev/rest/springrestapi/Application.java
+++ src/main/java/dev/rest/springrestapi/Application.java
@@ -3,6 +3,8 @@ package dev.rest.springrestapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/dev/rest/springrestapi/controller/ContentJdbcTemplateRepository.java
@@ -0,0 +1,4 @@
+package dev.rest.springrestapi.controller;
+
+public class ContentJdbcTemplateRepository {
+}
